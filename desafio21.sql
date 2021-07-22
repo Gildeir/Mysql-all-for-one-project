4c9ced61d875dbd65bfa060c120b6afcb73496bc
Gildeir <gildeirlopes@gmail.com>
2021-07-22 18:31:27 -0300

    Corrigindo Code Climate

--- desafio21.sql
+++ desafio21.sql
@@ -1,5 +1,5 @@
 INSERT INTO northwind.order_details
-	(order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+  (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
 VALUES 
-	(69, 80, 15.000, 15.000, 0, 2, NULL, NULL, 129),
-    (69, 80, 15.000, 15.000, 0, 2, NULL, NULL, 129);
+  (69, 80, 15.000, 15.000, 0, 2, NULL, NULL, 129),
+  (69, 80, 15.000, 15.000, 0, 2, NULL, NULL, 129);
