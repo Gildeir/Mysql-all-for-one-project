8d535d7b99019913d2e3c6d8d19163815b4ae27f
Gildeir <gildeirlopes@gmail.com>
2021-07-22 15:41:55 -0300

    Code Climate requisito 5

---
+++ desafio6.sql
@@ -0,0 +1,5 @@
+-- 6 - Exiba os dados das colunas product_name e id da tabela products de maneira 
+-- que os resultados estejam em ordem alfabética dos nomes.
+
+SELECT product_name, id FROM northwind.products
+ORDER BY product_name;
